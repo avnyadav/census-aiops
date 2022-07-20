57dcf7d57aebf7ba9fc8de59e9b61308ab661a51
Deepranjan Kumar Gupta <47396584+DeepranjanG@users.noreply.github.com>
2022-07-21 00:11:23 +0530

    updated

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ REQUIREMENT_FILE_NAME = "requirements.txt"
 REMOVE_PACKAGE = "-e ."
 
 
-def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
-    try:
-        requirement_list = None
-        with open(requirement_file_name) as requirement_file:
-            requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
-            requirement_list.remove(REMOVE_PACKAGE)
-        return requirement_list
-    except Exception as e:
-        raise e
+# def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
+#     try:
+#         requirement_list = None
+#         with open(requirement_file_name) as requirement_file:
+#             requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
+#             requirement_list.remove(REMOVE_PACKAGE)
+#         return requirement_list
+#     except Exception as e:
+#         raise e
 
 
 
 setup(
     name="census-consumer-complaint",
     license="MIT",
-    version="0.0.8",
+    version="0.0.9",
     description="Project has been completed.",
     # packages=find_packages(),
     # install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
@@ -32,7 +32,7 @@ setup(
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=get_requirement_list()
+    install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
 )
 
 
