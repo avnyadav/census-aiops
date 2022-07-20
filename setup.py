78c41d50d8ce0f92369cf810a5dd6e2d8d3390b5
Deepranjan Kumar Gupta <47396584+DeepranjanG@users.noreply.github.com>
2022-07-21 00:06:47 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
 from setuptools import setup, find_packages
 
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+REMOVE_PACKAGE = "-e ."
+
+
+def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
+    try:
+        requirement_list = None
+        with open(requirement_file_name) as requirement_file:
+            requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
+            requirement_list.remove(REMOVE_PACKAGE)
+        return requirement_list
+    except Exception as e:
+        raise e
+
+
+
 setup(
     name="census-consumer-complaint",
     license="MIT",
-    version="0.0.7",
+    version="0.0.8",
     description="Project has been completed.",
-    author="Deepranjan",
+    # packages=find_packages(),
+    # install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
+    author="Avnish Yadav",
     packages=find_packages(),
-    install_requires=['tfx==1.6.1', 'apache-beam[interactive]', 'apache-airflow']
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=get_requirement_list()
 )
+
+
